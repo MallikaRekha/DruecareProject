dadee7655a05b22cdf71e5322f5215fc43c0cba2
MallikaRekha <101656209+MallikaRekha@users.noreply.github.com>
2022-06-02 10:56:53 +0530

    Spring_Boot_Configuration Commit

---
+++ spring-boot-configuration/src/test/java/io/javabrains/springbootconfiguration/SpringBootConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springbootconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
